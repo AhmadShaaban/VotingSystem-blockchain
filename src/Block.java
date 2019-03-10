af5b028ec5f08d0764f34d71683ae1584a4f9b88
AhmedMaherAli <45319458+AhmedMaherAli@users.noreply.github.com>
2019-03-10 18:46:50 +0200

    Add files via upload
    Just adding the Hachblock function in the block class to calc the hash using the data,time stamp, previous hash, etc

--- src/Block.java
+++ src/Block.java
@@ -1,78 +1,20 @@
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.util.Date;
-
-    public  class Block {
-        public int index;
-        private long timestamp;
-        private String Hash;
-        private String PrevHash;
-        private String data;
-        private int nonce = 1;
-        public static  int diff ;
-        public static int cnt = 0;
-
-
-
-
-
-
-
-        //setters
-        public void setIndex(int index) {
-            this.index = index;
+public  String HashBlock(){
+        MessageDigest digest;
+        String temp = Integer.toString(nonce) + data + timestamp + PrevHash;
+        String EncodedString = new String();
+        try {
+             digest = MessageDigest.getInstance("SHA-256");
+            byte [] EncodedMessage = digest.digest(temp.getBytes(StandardCharsets.UTF_8));
+ 
+ 
+            for (int i = 0 ; i < EncodedMessage.length; ++i){
+                String t = new String();
+                t = Integer.toHexString(0xff&EncodedMessage[i]);
+                if (t.length()==1)
+                    t+="0";
+                EncodedString +=t;
+            }
         }
-
-        public void setTimestamp(long timestamp) {
-            this.timestamp = timestamp;
-        }
-
-        public void setHash (){
-            Hash = HashBlock();
-        }
-
-        public void setPrevHash(String prevHash) {
-            PrevHash = prevHash;
-        }
-
-        public int getIndex() {
-            return index;
-        }
-
-        public void setData(String data) {
-            this.data = data;
-        }
-
-        public void setNonce(int nonce) {
-            this.nonce = nonce;
-        }
-
-        //getters
-
-        public int getNonce() {
-            return nonce;
-        }
-
-        public long getTimestamp() {
-            return timestamp;
-        }
-
-        public String getData() {
-            return data;
-        }
-
-        public String getHash() {
-            return Hash;
-        }
-
-        public String getPrevHash() {
-            return PrevHash;
-        }
-
-
-
-
-
+        catch (Exception e){}
+        return EncodedString;
     }
-
-
