5548520ae6244ba66660a2d6edcdaf59ae3e4fa8
AhmedMaherAli <45319458+AhmedMaherAli@users.noreply.github.com>
2019-03-10 19:34:48 +0200

    The Trie class attributes
    It is a built-in data structure at many languages but not supported at java and we gonna use it,so we implemented it manually.

--- src/Trie.java
+++ src/Trie.java
@@ -1,2 +1,12 @@
+import java.security.PublicKey;
+ 
 public class Trie {
+    public PublicKey first;
+    public byte[] second;
+    public String third;
+    Trie(PublicKey first,byte[] second,String third){
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
 }
